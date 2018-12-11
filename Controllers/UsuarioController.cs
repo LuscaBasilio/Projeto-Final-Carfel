455375cf93a8baaf9dd261bbb3c782832100f948
FIC <FIC@info.local>
2018-12-11 11:26:48 -0200

    Commit inicial

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,7 @@
+namespace Projeto_FINALA.Controllers
+{
+    public class UsuarioController
+    {
+        //WIP - O que foi feito aqui também foi devidamente descartado por não estar funcionando ;-;
+    }
+}
---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Projeto_FINALA.Models
+{
+    public class UsuarioModel
+    {
+        public string Nome;
+        public string Email;
+        public string Senha;
+        public int Id;
+        public DateTime DataCriacao;
+        public bool Tipo;
+    }
+}
